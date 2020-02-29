4992345d175f1cc6d30885b24ad6e8b81ea3198e
mariuszprokop88 <mariuszprokop@outlook.com>
2020-02-29 13:35:47 +0100

    Task7. Saving to file and loading file content implemented.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,15 +7,21 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+    private static Stage mainStage;
     String resourcePath = "/fxml/Main.fxml";
     @Override
     public void start(Stage primaryStage) throws Exception {
+        mainStage = primaryStage;
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource(resourcePath));
         AnchorPane anchor = loader.load();
         Scene scene = new Scene(anchor);
         primaryStage.setTitle("Enigma");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static Stage getMainStage() {
+        return mainStage;
     }
 
 
--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -2,22 +2,30 @@ package controllers;
 
 import ciphers.Cipher;
 import ciphers.impl.CesarCipher;
+import file.utils.FileTool;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+    private Stage mainStage;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
     @FXML
     public TextArea inputTextArea;
+
 
 
     @FXML
@@ -38,4 +46,35 @@ public class MainController implements Initializable {
             inputTextArea.setText(decode);
         }
     }
-}
+    @FXML
+    public void saveToFile(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save to file");
+        fileChooser.setInitialFileName("message.crpt");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("crypt file", "*.crpt"),
+                new FileChooser.ExtensionFilter("text file", "*txt")
+        );
+        File fileToSave = fileChooser.showSaveDialog(null);
+        if(fileToSave!= null) {
+            String absolutePath = fileToSave.getAbsolutePath();
+            FileTool.writeTextAreaContentToFile(absolutePath, inputTextArea.getText());
+        }
+    }
+
+@FXML
+public void loadFile(){
+    FileChooser fileChooser = new FileChooser();
+    fileChooser.setTitle("Load file");
+    fileChooser.getExtensionFilters().addAll(
+            new FileChooser.ExtensionFilter("crypt file", "*.crpt"),
+            new FileChooser.ExtensionFilter("text file", "*.txt")
+    );
+    File fileToLoad = fileChooser.showOpenDialog(null);
+    if(null!=fileToLoad) {
+    String fileContent = FileTool.getFileContent(fileToLoad.getAbsolutePath());
+    inputTextArea.setText(fileContent);
+
+        }
+    }}
+
