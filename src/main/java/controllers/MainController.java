5f2752d6b18cd7c6823e6f3718712904075e2dfc
mariuszprokop88 <mariuszprokop@outlook.com>
2020-03-01 11:08:44 +0100

    Task-8

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -3,10 +3,15 @@ package controllers;
 import aplication.Main;
 import ciphers.Cipher;
 import ciphers.impl.CesarCipher;
+import ciphers.impl.Root13Cipher;
+import ciphers.impl.VigenereCipher;
+import factories.impl.CipherFactory;
 import file.utils.FileTool;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -16,36 +21,71 @@ import java.io.File;
 import java.io.IOError;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.*;
+
+import static factories.impl.CipherFactory.*;
 
 public class MainController implements Initializable {
     private Stage mainStage;
+    private ObservableList<String> possibleCipherMethods =  FXCollections.observableArrayList(Arrays.asList(CESAR, ROOT13, VIGENERE));
+    private CipherFactory cipherFactory = new CipherFactory();
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        cipherChoiceBox.setItems(possibleCipherMethods);
+        cipherChoiceBox.setValue(CESAR);
         mainStage = Main.getMainStage();
+
     }
     @FXML
     public TextArea inputTextArea;
 
+    @FXML
+    ChoiceBox<String>cipherChoiceBox;
+
 
 
     @FXML
-    public void triggerEncoding(){
+    public void triggerEncoding() {
         String userText = inputTextArea.getText();
-        if(!userText.isEmpty()){
-            Cipher cesarCipher = new CesarCipher();
-            String encode = cesarCipher.encode(userText);
+        if (!userText.isEmpty()) {
+            String cipherType = cipherChoiceBox.getValue();
+            Cipher cipher = cipherFactory.create(cipherType);
+            if (cipher instanceof VigenereCipher) {
+                TextInputDialog inputDialog = new TextInputDialog("Key");
+                inputDialog.setHeaderText("Pleas choose key and rememebr it");
+                inputDialog.setContentText("Key: ");
+                Optional<String> userInputOptional = inputDialog.showAndWait();
+                if (userInputOptional.isPresent() && !userInputOptional.get().equals("")) {
+                    ((VigenereCipher) cipher).setKey(userInputOptional.get());
+                } else {
+                    return;
+                }
+            }
+            String encode = cipher.encode(userText);
             inputTextArea.setText(encode);
         }
     }
 
+
     public void triggerDecoding(){
         String userText = inputTextArea.getText();
-        if(!userText.isEmpty()){
-            Cipher cesarCipher = new CesarCipher();
-            String decode = cesarCipher.decode(userText);
+        if (!userText.isEmpty()) {
+            String cipherType = cipherChoiceBox.getValue();
+            Cipher cipher = cipherFactory.create(cipherType);
+            if (cipher instanceof VigenereCipher) {
+                TextInputDialog inputDialog = new TextInputDialog("Key");
+                inputDialog.setHeaderText("If you know the key, write it here");
+                inputDialog.setContentText("Key: ");
+                Optional<String> userInputOptional = inputDialog.showAndWait();
+                if (userInputOptional.isPresent() && !userInputOptional.get().equals("")) {
+                    ((VigenereCipher) cipher).setKey(userInputOptional.get());
+                } else {
+                    return;
+                }
+            }
+            String decode = cipher.decode(userText);
             inputTextArea.setText(decode);
         }
     }
--- src/main/java/factories/impl/CipherFactory.java
+++ src/main/java/factories/impl/CipherFactory.java
@@ -3,11 +3,14 @@ package factories.impl;
 import ciphers.Cipher;
 import ciphers.impl.CesarCipher;
 import ciphers.impl.Root13Cipher;
+import ciphers.impl.VigenereCipher;
 import exceptions.CipherNotFoundException;
 
 public class CipherFactory implements factories.CipherFactory {
     public static final String CESAR = "cipher";
     public static final String ROOT13 = "root13";
+    public static final String VIGENERE = "vigenere";
+
     @Override
     public Cipher create(String type) {
         if(type.equals(CESAR)) {
@@ -15,6 +18,9 @@ public class CipherFactory implements factories.CipherFactory {
         }
         if(type.equals(ROOT13)) {
             return new Root13Cipher();
+        }
+        if(type.equals(VIGENERE)) {
+            return new VigenereCipher();
 
         }throw new CipherNotFoundException(type);
     }
